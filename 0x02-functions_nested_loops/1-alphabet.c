7e65f8a9b834918b14e5814bff1752e1b1ab8111
Salma <salma1soliman00@gmail.com>
2023-02-21 15:35:05 -0800

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * main program
+ * Return: always 0
+ *
+ */
+void print_alphabet(void)
+{
+	int c;
+	for(c='a';c<='z';c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
