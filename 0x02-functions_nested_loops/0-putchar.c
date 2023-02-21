d35d9bb57938c028011db8a0dc86cea0d4cc7296
Salma <salma1soliman00@gmail.com>
2023-02-21 15:25:02 -0800

    c language

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * is written by salma
  * Return 0
  */
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,5 +16,5 @@ int main (void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
