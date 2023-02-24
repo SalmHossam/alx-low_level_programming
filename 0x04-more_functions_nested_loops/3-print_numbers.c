d36f35f988c78430d6a62d9bad517418ed1dd258
Salma <salma1soliman00@gmail.com>
2023-02-24 15:49:31 -0800

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdio.h>
+
+/***
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * Return : numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+
+	int i;
+	for (i = 0 ; i <= 9 ; i++)
+	{
+	_putchar(i + '0');
+	}
+	_putchar('\n');
+}
+
