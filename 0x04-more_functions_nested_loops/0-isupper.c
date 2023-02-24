4be96ee1f93ec5ed574b8db6208fbd21b8db2aa6
Salma <salma1soliman00@gmail.com>
2023-02-24 03:26:15 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper check if the digit id upper case or not
+ * Return 1 for uppercase else Return 0
+ */
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return(1);
+	}
+	return(0);
+}
