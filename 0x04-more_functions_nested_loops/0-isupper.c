a12bbe6e7a396c908321a922fdece6d442a0ca34
Salma <salma1soliman00@gmail.com>
2023-02-24 04:02:31 -0800

    betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,16 @@
 
 /**
  * _isupper check if the digit id upper case or not
+ *
  * Return 1 for uppercase else Return 0
  */
-int _isupper(int c)
+
+int _isupper (int c)
 {
 
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
-	return(1);
+	return (1);
 	}
-	return(0);
+	return (0);
 }
