30a8b75564d4ab42bc2bee281fa37e205e3e4abb
Salma <salma1soliman00@gmail.com>
2023-02-24 04:15:38 -0800

    betty

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Check if the digit is a digit or not
+ *@c: The number to be checked
+ * Return: 1 for uppercase else Return: 0
+ */
+
+int _isdigit(int c)
+{
+
+        if (c >= 48 && c <= 57)
+        {
+        return (1);
+        }
+        return (0);
+}
