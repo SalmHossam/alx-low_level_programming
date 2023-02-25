5eaad1c321c50edb0d805e315729830622447b0d
Salma <salma1soliman00@gmail.com>
2023-02-24 16:59:57 -0800

    more_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,13 +7,14 @@
  * Return : numbers from 0 to 9
  */
 
+
 void print_numbers(void)
 {
-
+             
 	char i;
 	for (i = '0' ; i <= '9' ; i++)
 	{
-	_putchar( i );
+	_putchar(i);
 	}
 	_putchar('\n');
 }
