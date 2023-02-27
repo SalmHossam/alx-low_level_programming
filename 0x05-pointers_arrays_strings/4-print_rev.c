140f307bbc9099c04363a16c70167c50998570cd
Salma <salma1soliman00@gmail.com>
2023-02-27 15:03:49 -0800

    put

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * print_rev - This function print the reverse of string
+ * @str: Parameter string of function
+ * Return: void
+ */
+void print_rev(char *s)
+{
+
+	int i = s.size();
+	string str= "";
+
+	for (; i>=0; i--)
+	{
+	str+=s[i];
+	}
+}
+
