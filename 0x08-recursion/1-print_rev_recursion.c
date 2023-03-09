abfd348302512653f3be79497f4790d961016f3c
Salma <salma1soliman00@gmail.com>
2023-03-08 16:37:11 -0800

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - Function reverse string using recursion
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0') 
+    {
+    return ;
+    }
+    _print_rev_recursion(s + 1);
+    putchar(*s);
+}
